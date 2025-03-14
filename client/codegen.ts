28c8b94a8be7df745e49a1bda75ffc00dab28845
Summer-Children <nogata00@mylangara.ca>
2025-03-14 04:01:52 -0700

    feat:add codegen.file

---
+++ client/codegen.ts
@@ -0,0 +1,17 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "http://localhost:4000/graphql",
+  documents: ["./src/**/*.{ts,tsx}"],
+  generates: {
+    "./src/_gqlgen/": {
+      preset: "client",
+    },
+  },
+  config: {
+    useTypeImports: true,
+  },
+};
+
+export default config;
